7846def08446500f734b774a8ab9ffc7e53ca455
Sherk-cell <70703520+Sherk-cell@users.noreply.github.com>
2022-06-07 22:25:18 +0200

    RGB game added

---
+++ Assets/Scripts/Screentaker.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Screentaker : MonoBehaviour
+{
+    public Colour_game Screen1;
+    public Colour_game Screen2;
+    public Colour_game Screen3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Screen1.won == true && Screen2.won == true && Screen3.won == true)
+        {
+            Debug.Log("WIN!");
+        }
+    }
+}
